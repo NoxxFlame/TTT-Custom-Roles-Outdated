5e0578a2ee0b1f866d61184dd6acbd339a20c652
Malivil <mlebson@outlook.com>
2020-04-04 09:33:19 -0400

    Fixed spawned zombies not being able to pickup weapons in the next pre-round. Closes #20

--- gamemodes/terrortown/gamemode/weaponry.lua
+++ gamemodes/terrortown/gamemode/weaponry.lua
@@ -22,7 +22,7 @@ function GM:PlayerCanPickupWeapon(ply, wep)
 		return false
 	elseif IsEquipment(wep) and wep.IsDropped and (not ply:KeyDown(IN_USE)) then
 		return false
-	elseif ply:GetZombie() and not ply:GetZombiePrime() and wep:GetClass() ~= "weapon_zom_claws" then
+	elseif ply:GetZombie() and not ply:GetZombiePrime() and wep:GetClass() ~= "weapon_zom_claws" and GetRoundState() == ROUND_ACTIVE then
 		return false
 	end
 
