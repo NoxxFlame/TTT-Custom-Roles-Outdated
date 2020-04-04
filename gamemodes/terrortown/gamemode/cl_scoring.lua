26c2f4fd0396d6afeb1a54c833d1bf9cdef4fc08
Malivil <mlebson@outlook.com>
2020-04-03 23:04:03 -0400

    Added support to the end of round board for the case where a player was hypnotized and then zombified, or vice versa

--- gamemodes/terrortown/gamemode/cl_scoring.lua
+++ gamemodes/terrortown/gamemode/cl_scoring.lua
@@ -49,44 +49,79 @@ local PT = LANG.GetParamTranslation
 local jesterkiller = ""
 local jestervictim = ""
 local jesterkillerrole = -1
-local hypnotised = ""
+local hypnotised = {}
 local revived = {}
 local zombified = {}
 local disconnected = {}
 local spawnedplayers = {}
+
+local function FindTableIndex(playerTable, value)
+	local index = 0;
+	for name in playerTable do
+		index = index + 1
+		if name == value then
+			return index
+		end
+	end
+	return index
+end
+
+local function InsertPlayerToTable(playerTable, name)
+	local tableIndex = FindTableIndex(playerTable, name)
+	if tableIndex <= 0 then
+		table.insert(playerTable, name)
+	end
+end
+
+local function InsertRevivedPlayer(name)
+	InsertPlayerToTable(revived, name)
+end
 
 net.Receive("TTT_JesterKiller", function(len)
 	jesterkiller = net.ReadString()
 	jestervictim = net.ReadString()
 	jesterkillerrole = net.ReadInt(6)
 	if jesterkillerrole >= 0 then
-		table.insert(revived, jestervictim)
+		InsertRevivedPlayer(jestervictim)
 	end
 end)
 
 net.Receive("TTT_Hypnotised", function(len)
-	hypnotised = net.ReadString()
-	table.insert(revived, hypnotised)
+	local name = net.ReadString()
+	InsertPlayerToTable(hypnotised, name)
+	InsertRevivedPlayer(name)
+
+	-- Remove any record of this player being zombified
+	local zomIndex = FindTableIndex(zombified, name)
+	if zomIndex > 0 then
+		table.remove(zombified, zomIndex)
+	end
 end)
 
 net.Receive("TTT_Defibrillated", function(len)
-	defibed = net.ReadString()
-	table.insert(revived, defibed)
+	local name = net.ReadString()
+	InsertRevivedPlayer(name)
 end)
 
 net.Receive("TTT_Zombified", function(len)
-	zomed = net.ReadString()
-	table.insert(zombified, zomed)
-	table.insert(revived, zomed)
+	local name = net.ReadString()
+	InsertPlayerToTable(zombified, name)
+	InsertRevivedPlayer(name)
+
+	-- Remove any record of this player being hypnotized
+	local hypIndex = FindTableIndex(hypnotised, name)
+	if hypIndex > 0 then
+		table.remove(hypnotised, hypIndex)
+	end
 end)
 
 net.Receive("TTT_PlayerDisconnected", function(len)
-	disconned = net.ReadString()
-	table.insert(disconnected, disconned)
+	local name = net.ReadString()
+	table.insert(disconnected, name)
 end)
 
 net.Receive("TTT_ClearRoleSwaps", function(len)
-	hypnotised = ""
+	hypnotised = {}
 	revived = {}
 	zombified = {}
 	disconnected = {}
@@ -94,8 +129,8 @@ net.Receive("TTT_ClearRoleSwaps", function(len)
 end)
 
 net.Receive("TTT_SpawnedPlayers", function(len)
-	spawnedPly = net.ReadString()
-	table.insert(spawnedplayers, spawnedPly)
+	local name = net.ReadString()
+	table.insert(spawnedplayers, name)
 end)
 
 function CLSCORE:GetDisplay(key, event)
@@ -103,7 +138,7 @@ function CLSCORE:GetDisplay(key, event)
 	if not displayfns then return end
 	local keyfn = displayfns[key]
 	if not keyfn then return end
-	
+
 	return keyfn(event)
 end
 
@@ -141,18 +176,18 @@ function CLSCORE.DeclareEventDisplay(event_id, event_fns)
 	if not event_fns.icon then
 		Error(Format("Event %d display: no icon and tooltip display function found.\n", event_id))
 	end
-	
+
 	CLSCORE.EventDisplay[event_id] = event_fns
 end
 
 function CLSCORE:FillDList(dlst)
-	
+
 	for k, e in pairs(self.Events) do
-		
+
 		local etxt = self:TextForEvent(e)
 		local eicon, ttip = self:IconForEvent(e)
 		local etime = self:TimeForEvent(e)
-		
+
 		if etxt then
 			if eicon then
 				local mat = eicon
@@ -162,7 +197,7 @@ function CLSCORE:FillDList(dlst)
 				eicon:SetKeepAspect(true)
 				eicon:SizeToContents()
 			end
-			
+
 			dlst:AddLine(etime, eicon, "  " .. etxt)
 		end
 	end
@@ -170,41 +205,41 @@ end
 
 function CLSCORE:BuildEventLogPanel(dpanel)
 	local margin = 10
-	
+
 	local w, h = dpanel:GetSize()
-	
+
 	local dlist = vgui.Create("DListView", dpanel)
 	dlist:SetPos(0, 0)
 	dlist:SetSize(w, h - margin * 2)
 	dlist:SetSortable(true)
 	dlist:SetMultiSelect(false)
-	
+
 	local timecol = dlist:AddColumn(T("col_time"))
 	local iconcol = dlist:AddColumn("")
 	local eventcol = dlist:AddColumn(T("col_event"))
-	
+
 	iconcol:SetFixedWidth(16)
 	timecol:SetFixedWidth(40)
-	
+
 	-- If sortable is off, no background is drawn for the headers which looks
 	-- terrible. So enable it, but disable the actual use of sorting.
 	iconcol.Header:SetDisabled(true)
 	timecol.Header:SetDisabled(true)
 	eventcol.Header:SetDisabled(true)
-	
+
 	self:FillDList(dlist)
 end
 
 function CLSCORE:BuildScorePanel(dpanel)
 	local margin = 10
 	local w, h = dpanel:GetSize()
-	
+
 	local dlist = vgui.Create("DListView", dpanel)
 	dlist:SetPos(0, 0)
 	dlist:SetSize(w, h)
 	dlist:SetSortable(true)
 	dlist:SetMultiSelect(false)
-	
+
 	local colnames = { "", "col_player", "col_role", "col_kills1", "col_kills2", "col_points", "col_team", "col_total" }
 	for k, name in pairs(colnames) do
 		if name == "" then
@@ -215,7 +250,7 @@ function CLSCORE:BuildScorePanel(dpanel)
 			dlist:AddColumn(T(name))
 		end
 	end
-	
+
 	-- the type of win condition triggered is relevant for team bonus
 	local wintype = WIN_NONE
 	for i = #self.Events, 1, -1 do
@@ -225,41 +260,41 @@ function CLSCORE:BuildScorePanel(dpanel)
 			break
 		end
 	end
-	
+
 	local scores = self.Scores
 	local nicks = self.Players
 	local bonus = ScoreTeamBonus(scores, wintype)
-	
+
 	for id, s in pairs(scores) do
 		if id ~= -1 then
 			local was_traitor = s.was_traitor
 			local role = was_traitor and T("traitor") or (s.was_detective and T("detective") or (s.was_hypnotist and T("hypnotist") or (s.was_mercenary and T("mercenary") or (s.was_jester and T("jester") or (s.was_phantom and T("phantom") or (s.was_glitch and T("glitch") or (s.was_zombie and T("zombie") or (s.was_vampire and T("vampire") or (s.was_swapper and T("swapper") or (s.was_assassin and T("assassin") or (s.was_killer and T("killer") or T("innocent"))))))))))))
-			
+
 			local surv = ""
 			if s.deaths > 0 then
 				surv = vgui.Create("ColoredBox", dlist)
 				surv:SetColor(Color(150, 50, 50))
 				surv:SetBorder(false)
 				surv:SetSize(18, 18)
-				
+
 				local skull = vgui.Create("DImage", surv)
 				skull:SetMaterial(skull_icon)
 				skull:SetTooltip("Dead")
 				skull:SetKeepAspect(true)
 				skull:SetSize(18, 18)
 			end
-			
+
 			local points_own = KillsToPoints(s, was_traitor)
 			local points_team = (was_traitor and bonus.traitors or bonus.innos)
 			local points_total = points_own + points_team
-			
+
 			local l = dlist:AddLine(surv, nicks[id], role, s.innos, s.traitors, points_own, points_team, points_total)
-			
+
 			-- center align
 			for k, col in pairs(l.Columns) do
 				col:SetContentAlignment(5)
 			end
-			
+
 			-- when sorting on the column showing survival, we would get an error
 			-- because images can't be sorted, so instead hack in a dummy value
 			local surv_col = l.Columns[1]
@@ -268,7 +303,7 @@ function CLSCORE:BuildScorePanel(dpanel)
 			end
 		end
 	end
-	
+
 	dlist:SortByColumn(6)
 end
 
@@ -276,35 +311,35 @@ function CLSCORE:AddAward(y, pw, award, dpanel)
 	local nick = award.nick
 	local text = award.text
 	local title = string.upper(award.title)
-	
+
 	local titlelbl = vgui.Create("DLabel", dpanel)
 	titlelbl:SetText(title)
 	titlelbl:SetFont("TabLarge")
 	titlelbl:SizeToContents()
 	local tiw, tih = titlelbl:GetSize()
-	
+
 	local nicklbl = vgui.Create("DLabel", dpanel)
 	nicklbl:SetText(nick)
 	nicklbl:SetFont("DermaDefaultBold")
 	nicklbl:SizeToContents()
 	local nw, nh = nicklbl:GetSize()
-	
+
 	local txtlbl = vgui.Create("DLabel", dpanel)
 	txtlbl:SetText(text)
 	txtlbl:SetFont("DermaDefault")
 	txtlbl:SizeToContents()
 	local tw, th = txtlbl:GetSize()
-	
+
 	titlelbl:SetPos((pw - tiw) / 2, y)
 	y = y + tih + 2
-	
+
 	local fw = nw + tw + 5
 	local fx = ((pw - fw) / 2)
 	nicklbl:SetPos(fx, y)
 	txtlbl:SetPos(fx + nw + 5, y)
-	
+
 	y = y + nh
-	
+
 	return y
 end
 
@@ -323,7 +358,7 @@ local wintitle = {
 
 function CLSCORE:ShowPanel()
 	local margin = 15
-	
+
 	local dpanel = vgui.Create("DFrame")
 	local w, h = 700, 502
 	dpanel:SetSize(w, h)
@@ -334,20 +369,20 @@ function CLSCORE:ShowPanel()
 	dpanel:SetMouseInputEnabled(true)
 	dpanel:SetKeyboardInputEnabled(true)
 	dpanel.OnKeyCodePressed = util.BasicKeyHandler
-	
+
 	function dpanel:Think()
 		self:MoveToFront()
 	end
-	
+
 	-- keep it around so we can reopen easily
 	dpanel:SetDeleteOnClose(false)
 	self.Panel = dpanel
-	
+
 	local bg = vgui.Create("ColoredBox", dpanel)
 	bg:SetColor(Color(97, 100, 102, 255))
 	bg:SetSize(w - 4, h - 26)
 	bg:SetPos(2, 24)
-	
+
 	local title = wintitle[WIN_INNOCENT]
 	for i = #self.Events, 1, -1 do
 		local e = self.Events[i]
@@ -358,7 +393,7 @@ function CLSCORE:ShowPanel()
 			break
 		end
 	end
-	
+
 	local winlbl = vgui.Create("DLabel", dpanel)
 	winlbl:SetFont("WinHuge")
 	winlbl:SetText(T(title.txt))
@@ -367,7 +402,7 @@ function CLSCORE:ShowPanel()
 	local xwin = (w - winlbl:GetWide()) / 2
 	local ywin = 37
 	winlbl:SetPos(xwin, ywin)
-	
+
 	bg.PaintOver = function()
 		draw.RoundedBox(8, 8, 8, 680, winlbl:GetTall() + 10, title.c)
 		draw.RoundedBox(0, 8, ywin - 19 + winlbl:GetTall() + 8, 336, 329, Color(164, 164, 164, 255))
@@ -378,17 +413,17 @@ function CLSCORE:ShowPanel()
 			draw.RoundedBox(0, 352, i, 336, 1, Color(97, 100, 102, 255))
 		end
 	end
-	
+
 	local scores = self.Scores
 	local nicks = self.Players
 	local symbols = false
 	local countI = 0
 	local countT = 0
-	
+
 	for id, s in pairs(scores) do
 		if id ~= -1 then
 			local role = s.was_traitor and "tra" or (s.was_detective and "det" or (s.was_hypnotist and "hyp" or (s.was_jester and "jes" or (s.was_swapper and "swa" or (s.was_mercenary and "mer" or (s.was_glitch and "gli" or (s.was_phantom and "pha" or (s.was_zombie and "zom" or (s.was_assassin and "ass" or (s.was_vampire and "vam" or (s.was_killer and "kil" or "inn")))))))))))
-			
+
 			if role == "swa" and jesterkillerrole >= 0 then
 				if jesterkillerrole == 0 then
 					role = "inn"
@@ -418,66 +453,68 @@ function CLSCORE:ShowPanel()
 					role = "kil"
 				end
 			end
-			
+
 			local foundPlayer = false
-			
+
 			for k, v in pairs(spawnedplayers) do
 				if v == nicks[id] then
 					foundPlayer = true
 					break
 				end
 			end
-			
+
 			if foundPlayer then
 				local dead = s.deaths
 				local hasDisconnected = false
-				
+
 				for k, v in pairs(revived) do
 					if v == nicks[id] then
 						dead = dead - 1
 					end
 				end
-				
+
 				for k, v in pairs(disconnected) do
 					if v == nicks[id] then
 						hasDisconnected = true
 						break
 					end
 				end
-				
+
 				if nicks[id] == jesterkiller and jesterkillerrole >= 0 then
 					role = "swa"
 				end
-				
+
 				if ConVarExists("ttt_role_symbols") then
 					symbols = GetConVar("ttt_role_symbols"):GetBool()
 				end
-				
+
 				local symorlet = "let"
 				if symbols then
 					symorlet = "sym"
 				end
-				
-				if nicks[id] == hypnotised then
-					role = role .. "_hyped"
+
+				for k, v in pairs(hypnotised) do
+					if v == nicks[id] then
+						role = role .. "_hyped"
+					end
 				end
-				
+
 				for k, v in pairs(zombified) do
 					if v == nicks[id] then
 						role = role .. "_zomed"
 					end
 				end
-				
+
 				local roleIcon = vgui.Create("DImage", dpanel)
 				roleIcon:SetSize(32, 32)
 				roleIcon:SetImage("vgui/ttt/score_" .. symorlet .. "_" .. role .. ".png")
-				
+
 				local nicklbl = vgui.Create("DLabel", dpanel)
 				nicklbl:SetFont("ScoreNicks")
 				nicklbl:SetText(nicks[id])
 				nicklbl:SetTextColor(COLOR_WHITE)
 				nicklbl:SizeToContents()
-				
+
 				if role == "inn" or role == "det" or role == "mer" or role == "pha" or role == "gli" then
 					roleIcon:SetPos(10, 123 + 33 * countI)
 					nicklbl:SetPos(48, 121 + 33 * countI)
@@ -535,18 +572,18 @@ function CLSCORE:ShowPanel()
 			end
 		end
 	end
-	
+
 	dpanel:MakePopup()
-	
+
 	-- makepopup grabs keyboard, whereas we only need mouse
 	dpanel:SetKeyboardInputEnabled(false)
 end
 
 function CLSCORE:ClearPanel()
-	
+
 	if self.Panel then
 		-- move the mouse off any tooltips and then remove the panel next tick
-		
+
 		-- we need this hack as opposed to just calling Remove because gmod does
 		-- not offer a means of killing the tooltip, and doesn't clean it up
 		-- properly on Remove
@@ -561,17 +598,17 @@ function CLSCORE:SaveLog()
 		chat.AddText(COLOR_WHITE, T("report_save_error"))
 		return
 	end
-	
+
 	local logdir = "ttt/logs"
 	if not file.IsDir(logdir, "DATA") then
 		file.CreateDir(logdir)
 	end
-	
+
 	local logname = logdir .. "/ttt_events_" .. os.time() .. ".txt"
 	local log = "Trouble in Terrorist Town - Round Events Log\n" .. string.rep("-", 50) .. "\n"
-	
+
 	log = log .. string.format("%s | %-25s | %s\n", " TIME", "TYPE", "WHAT HAPPENED") .. string.rep("-", 50) .. "\n"
-	
+
 	for _, e in pairs(self.Events) do
 		local etxt = self:TextForEvent(e)
 		local etime = self:TimeForEvent(e)
@@ -580,9 +617,9 @@ function CLSCORE:SaveLog()
 			log = log .. string.format("%s | %-25s | %s\n", etime, etype, etxt)
 		end
 	end
-	
+
 	file.Write(logname, log)
-	
+
 	chat.AddText(COLOR_WHITE, T("report_save_result"), COLOR_GREEN, " /garrysmod/data/" .. logname)
 end
 
@@ -604,7 +641,7 @@ function CLSCORE:Reset()
 	self.Scores = {}
 	self.Players = {}
 	self.RoundStarted = 0
-	
+
 	self:ClearPanel()
 end
 
@@ -640,12 +677,12 @@ function CLSCORE:Init(events)
 			assassin = e.assassin_ids
 			killer = e.killer_ids
 		end
-		
+
 		if starttime and traitors then
 			break
 		end
 	end
-	
+
 	-- Get scores and players
 	local scores = {}
 	local nicks = {}
@@ -655,9 +692,9 @@ function CLSCORE:Init(events)
 			nicks[e.sid] = e.ni
 		end
 	end
-	
+
 	scores = ScoreEventLog(events, scores, traitors, detectives, hypnotist, mercenary, jester, phantom, glitch, zombie, vampire, swapper, assassin, killer)
-	
+
 	self.Players = nicks
 	self.Scores = scores
 	self.TraitorIDs = traitors
@@ -678,7 +715,7 @@ end
 
 function CLSCORE:ReportEvents(events)
 	self:Reset()
-	
+
 	self:Init(events)
 	self:ShowPanel()
 end
@@ -692,28 +729,28 @@ end
 local buff = ""
 local function ReceiveReportStream(len)
 	local cont = net.ReadBit() == 1
-	
+
 	buff = buff .. net.ReadString()
-	
+
 	if cont then
 		return
 	else
 		-- do stuff with buffer contents
-		
+
 		local json_events = buff -- util.Decompress(buff)
 		if not json_events then
 			ErrorNoHalt("Round report decompression failed!\n")
 		else
 			-- convert the json string back to a table
 			local events = util.JSONToTable(json_events)
-			
+
 			if istable(events) then
 				CLSCORE:ReportEvents(events)
 			else
 				ErrorNoHalt("Round report event decoding failed!\n")
 			end
 		end
-		
+
 		-- flush
 		buff = ""
 	end
