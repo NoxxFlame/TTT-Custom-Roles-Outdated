c49d23ac025579e9f9aa67538dd9042d6eb32911
Thomas Richards <Deskbot@users.noreply.github.com>
2020-06-13 18:48:42 +0100

    Notify players when it changes

--- gamemodes/terrortown/gamemode/init.lua
+++ gamemodes/terrortown/gamemode/init.lua
@@ -152,7 +152,7 @@ CreateConVar("ttt_namechange_bantime", "10")
 
 CreateConVar("ttt_karma_beta", "0", FCVAR_REPLICATED)
 
-CreateConVar("ttt_independent_warn", "0")
+CreateConVar("ttt_independent_warn", "0", FCVAR_NOTIFY)
 
 -- Drinking game punishments
 CreateConVar("ttt_drinking_death", "drink")
