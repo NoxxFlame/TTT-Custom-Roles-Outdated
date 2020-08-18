9fb21803d36f7dada190c77151266d1a8f1d8d23
Malivil <mlebson@outlook.com>
2020-08-18 16:37:17 -0400

    Merge radio animation fix from TTT

--- Source/gamemodes/terrortown/gamemode/player_ext_shd.lua
+++ Source/gamemodes/terrortown/gamemode/player_ext_shd.lua
@@ -1,5 +1,4 @@
 -- shared extensions to player table
-
 local plymeta = FindMetaTable("Player")
 if not plymeta then return end
 
@@ -82,7 +81,7 @@ end
 
 -- Player is alive and in an active round
 function plymeta:IsActive()
-	return self:IsTerror() and GetRoundState() == ROUND_ACTIVE
+    return self:IsTerror() and GetRoundState() == ROUND_ACTIVE
 end
 
 -- convenience functions for common patterns
@@ -122,13 +121,11 @@ local GetRTranslation = CLIENT and LANG.GetRawTranslation or util.passthrough
 
 -- Returns printable role
 function plymeta:GetRoleString()
-	return GetRTranslation(ROLE_STRINGS[self:GetRole()]) or "???"
+    return GetRTranslation(ROLE_STRINGS[self:GetRole()]) or "???"
 end
 
 -- Returns role language string id, caller must translate if desired
-function plymeta:GetRoleStringRaw()
-	return ROLE_STRINGS[self:GetRole()]
-end
+function plymeta:GetRoleStringRaw() return ROLE_STRINGS[self:GetRole()] end
 
 function plymeta:GetBaseKarma() return self:GetNWFloat("karma", 1000) end
 
@@ -137,38 +134,32 @@ function plymeta:GetBaseDrinks() return self:GetNWInt("drinks", 0) end
 function plymeta:GetBaseShots() return self:GetNWInt("shots", 0) end
 
 function plymeta:HasEquipmentWeapon()
-	for _, wep in pairs(self:GetWeapons()) do
-		if IsValid(wep) and wep:IsEquipment() then
-			return true
-		end
-	end
+    for _, wep in pairs(self:GetWeapons()) do
+        if IsValid(wep) and wep:IsEquipment() then return true end
+    end
 
-	return false
+    return false
 end
 
 function plymeta:CanCarryWeapon(wep)
-	if (not wep) or (not wep.Kind) then return false end
+    if (not wep) or (not wep.Kind) then return false end
 
-	return self:CanCarryType(wep.Kind)
+    return self:CanCarryType(wep.Kind)
 end
 
 function plymeta:CanCarryType(t)
-	if not t then return false end
+    if not t then return false end
 
-	for _, w in pairs(self:GetWeapons()) do
-		if w.Kind and w.Kind == t then
-			return false
-		end
-	end
-	return true
+    for _, w in pairs(self:GetWeapons()) do
+        if w.Kind and w.Kind == t then return false end
+    end
+    return true
 end
 
-function plymeta:IsDeadTerror()
-	return (self:IsSpec() and not self:Alive())
-end
+function plymeta:IsDeadTerror() return (self:IsSpec() and not self:Alive()) end
 
 function plymeta:HasBought(id)
-	return self.bought and table.HasValue(self.bought, id)
+    return self.bought and table.HasValue(self.bought, id)
 end
 
 function plymeta:GetCredits() return self.equipment_credits or 0 end
@@ -178,15 +169,15 @@ function plymeta:GetEquipmentItems() return self.equipment_items or EQUIP_NONE e
 -- Given an equipment id, returns if player owns this. Given nil, returns if
 -- player has any equipment item.
 function plymeta:HasEquipmentItem(id)
-	if not id then
-		return self:GetEquipmentItems() ~= EQUIP_NONE
-	else
-		return util.BitSet(self:GetEquipmentItems(), id)
-	end
+    if not id then
+        return self:GetEquipmentItems() ~= EQUIP_NONE
+    else
+        return util.BitSet(self:GetEquipmentItems(), id)
+    end
 end
 
 function plymeta:HasEquipment()
-	return self:HasEquipmentItem() or self:HasEquipmentWeapon()
+    return self:HasEquipmentItem() or self:HasEquipmentWeapon()
 end
 
 -- Override GetEyeTrace for an optional trace mask param. Technically traces
@@ -196,14 +187,14 @@ function plymeta:GetEyeTrace(mask)
     mask = mask or MASK_SOLID
 
     if CLIENT then
-       local framenum = FrameNumber()
+        local framenum = FrameNumber()
 
-       if self.LastPlayerTrace == framenum and self.LastPlayerTraceMask == mask then
-          return self.PlayerTrace
-       end
+        if self.LastPlayerTrace == framenum and self.LastPlayerTraceMask == mask then
+            return self.PlayerTrace
+        end
 
-       self.LastPlayerTrace = framenum
-       self.LastPlayerTraceMask = mask
+        self.LastPlayerTrace = framenum
+        self.LastPlayerTraceMask = mask
     end
 
     local tr = util.GetPlayerTrace(self)
@@ -213,109 +204,99 @@ function plymeta:GetEyeTrace(mask)
     self.PlayerTrace = tr
 
     return tr
- end
+end
 
 if CLIENT then
 
-	function plymeta:AnimApplyGesture(act, weight)
-		self:AnimRestartGesture(GESTURE_SLOT_CUSTOM, act, true) -- true = autokill
-		self:AnimSetGestureWeight(GESTURE_SLOT_CUSTOM, weight)
-	end
+    function plymeta:AnimApplyGesture(act, weight)
+        self:AnimRestartGesture(GESTURE_SLOT_CUSTOM, act, true) -- true = autokill
+        self:AnimSetGestureWeight(GESTURE_SLOT_CUSTOM, weight)
+    end
 
-	local function MakeSimpleRunner(act)
-		return function(ply, w)
-			-- just let this gesture play itself and get out of its way
-			if w == 0 then
-				ply:AnimApplyGesture(act, 1)
-				return 1
-			else
-				return 0
-			end
-		end
-	end
+    local simple_runners = {
+        ACT_GMOD_GESTURE_DISAGREE, ACT_GMOD_GESTURE_BECON,
+        ACT_GMOD_GESTURE_AGREE, ACT_GMOD_GESTURE_WAVE, ACT_GMOD_GESTURE_BOW,
+        ACT_SIGNAL_FORWARD, ACT_SIGNAL_GROUP, ACT_SIGNAL_HALT,
+        ACT_GMOD_TAUNT_CHEER, ACT_GMOD_GESTURE_ITEM_PLACE,
+        ACT_GMOD_GESTURE_ITEM_DROP, ACT_GMOD_GESTURE_ITEM_GIVE
+    }
+    local function MakeSimpleRunner(act)
+        return function(ply, w)
+            -- just let this gesture play itself and get out of its way
+            if w == 0 then
+                ply:AnimApplyGesture(act, 1)
+                return 1
+            else
+                return 0
+            end
+        end
+    end
 
-	-- act -> gesture runner fn
-	local act_runner = {
-		-- ear grab needs weight control
-		-- sadly it's currently the only one
-		[ACT_GMOD_IN_CHAT] =
-		function(ply, w)
-			local dest = ply:IsSpeaking() and 1 or 0
-			w = math.Approach(w, dest, FrameTime() * 10)
-			if w > 0 then
-				ply:AnimApplyGesture(ACT_GMOD_IN_CHAT, w)
-			end
-			return w
-		end
-	};
+    -- act -> gesture runner fn
+    local act_runner = {
+        -- ear grab needs weight control
+        -- sadly it's currently the only one
+        [ACT_GMOD_IN_CHAT] = function(ply, w)
+            local dest = ply:IsSpeaking() and 1 or 0
+            w = math.Approach(w, dest, FrameTime() * 10)
+            if w > 0 then ply:AnimApplyGesture(ACT_GMOD_IN_CHAT, w) end
+            return w
+        end
+    };
 
-	-- Insert all the "simple" gestures that do not need weight control
-	for _, a in pairs {
-		ACT_GMOD_GESTURE_AGREE, ACT_GMOD_GESTURE_DISAGREE,
-		ACT_GMOD_GESTURE_WAVE, ACT_GMOD_GESTURE_BECON,
-		ACT_GMOD_GESTURE_BOW, ACT_GMOD_GESTURE_SALUTE,
-		ACT_GMOD_CHEER, ACT_SIGNAL_FORWARD, ACT_SIGNAL_HALT,
-		ACT_SIGNAL_GROUP, ACT_ITEM_PLACE, ACT_ITEM_DROP,
-		ACT_ITEM_GIVE
-	} do
-		act_runner[a] = MakeSimpleRunner(a)
-	end
+    -- Insert all the "simple" gestures that do not need weight control
+    for _, a in ipairs(simple_runners) do act_runner[a] = MakeSimpleRunner(a) end
 
-	CreateConVar("ttt_show_gestures", "1", FCVAR_ARCHIVE)
+    CreateConVar("ttt_show_gestures", "1", FCVAR_ARCHIVE)
 
-	-- Perform the gesture using the GestureRunner system. If custom_runner is
-	-- non-nil, it will be used instead of the default runner for the act.
-	function plymeta:AnimPerformGesture(act, custom_runner)
-		if GetConVarNumber("ttt_show_gestures") == 0 then return end
+    -- Perform the gesture using the GestureRunner system. If custom_runner is
+    -- non-nil, it will be used instead of the default runner for the act.
+    function plymeta:AnimPerformGesture(act, custom_runner)
+        if GetConVarNumber("ttt_show_gestures") == 0 then return end
 
-		local runner = custom_runner or act_runner[act]
-		if not runner then return false end
+        local runner = custom_runner or act_runner[act]
+        if not runner then return false end
 
-		self.GestureWeight = 0
-		self.GestureRunner = runner
+        self.GestureWeight = 0
+        self.GestureRunner = runner
 
-		return true
-	end
+        return true
+    end
 
-	-- Perform a gesture update
-	function plymeta:AnimUpdateGesture()
-		if self.GestureRunner then
-			self.GestureWeight = self:GestureRunner(self.GestureWeight)
+    -- Perform a gesture update
+    function plymeta:AnimUpdateGesture()
+        if self.GestureRunner then
+            self.GestureWeight = self:GestureRunner(self.GestureWeight)
 
-			if self.GestureWeight <= 0 then
-				self.GestureRunner = nil
-			end
-		end
-	end
+            if self.GestureWeight <= 0 then self.GestureRunner = nil end
+        end
+    end
 
-	function GM:UpdateAnimation(ply, vel, maxseqgroundspeed)
-		ply:AnimUpdateGesture()
+    function GM:UpdateAnimation(ply, vel, maxseqgroundspeed)
+        ply:AnimUpdateGesture()
 
-		return self.BaseClass.UpdateAnimation(self, ply, vel, maxseqgroundspeed)
-	end
+        return self.BaseClass.UpdateAnimation(self, ply, vel, maxseqgroundspeed)
+    end
 
-	function GM:GrabEarAnimation(ply) end
+    function GM:GrabEarAnimation(ply) end
 
-	net.Receive("TTT_PerformGesture", function()
-		local ply = net.ReadEntity()
-		local act = net.ReadUInt(16)
-		if IsValid(ply) and act then
-			ply:AnimPerformGesture(act)
-		end
-	end)
+    net.Receive("TTT_PerformGesture", function()
+        local ply = net.ReadEntity()
+        local act = net.ReadUInt(16)
+        if IsValid(ply) and act then ply:AnimPerformGesture(act) end
+    end)
 
 else -- SERVER
 
-	-- On the server, we just send the client a message that the player is
-	-- performing a gesture. This allows the client to decide whether it should
-	-- play, depending on eg. a cvar.
-	function plymeta:AnimPerformGesture(act)
+    -- On the server, we just send the client a message that the player is
+    -- performing a gesture. This allows the client to decide whether it should
+    -- play, depending on eg. a cvar.
+    function plymeta:AnimPerformGesture(act)
+        if not act then return end
 
-		if not act then return end
-
-		net.Start("TTT_PerformGesture")
-		net.WriteEntity(self)
-		net.WriteUInt(act, 16)
-		net.Broadcast()
-	end
+        net.Start("TTT_PerformGesture")
+        net.WriteEntity(self)
+        net.WriteUInt(act, 16)
+        net.Broadcast()
+    end
 end
