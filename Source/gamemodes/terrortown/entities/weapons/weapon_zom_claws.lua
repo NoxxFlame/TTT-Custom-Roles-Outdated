4c3ec15cfaa337553d91a62d01a526063280b9bc
Malivil <mlebson@outlook.com>
2020-09-25 19:11:01 -0400

    Updated Zombie claws to add support for Monsters-as-Traitors. For #117

--- Source/gamemodes/terrortown/entities/weapons/weapon_zom_claws.lua
+++ Source/gamemodes/terrortown/entities/weapons/weapon_zom_claws.lua
@@ -129,7 +129,7 @@ function SWEP:PrimaryAttack()
         self:GetOwner():SetAnimation(PLAYER_ATTACK1)
 
         if hitEnt and hitEnt:IsValid() then
-            if hitEnt:IsPlayer() and not hitEnt:IsZombie() and not hitEnt:IsVampire() then
+            if hitEnt:IsPlayer() and not hitEnt:IsMonsterAlly() then
                 if hitEnt:Health() <= self.Primary.Damage and not hitEnt:IsJester() and not hitEnt:IsSwapper() then
                     self:GetOwner():AddCredits(1)
                     LANG.Msg(self:GetOwner(), "credit_zom", { num = 1 })
@@ -161,6 +161,7 @@ function SWEP:PrimaryAttack()
                         SendFullStateUpdate()
                     end)
                 end
+
                 local dmg = DamageInfo()
                 dmg:SetDamage(self.Primary.Damage)
                 dmg:SetAttacker(self:GetOwner())
