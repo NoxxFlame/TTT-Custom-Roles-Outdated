beb5c8be5454b6616838791902614a39aa6f7f58
Malivil <mlebson@outlook.com>
2020-09-28 21:28:44 -0400

    Mising change for disguise support. For #117

--- Source/gamemodes/terrortown/gamemode/weaponry.lua
+++ Source/gamemodes/terrortown/gamemode/weaponry.lua
@@ -535,7 +535,7 @@ function GM:TTTToggleDisguiser(ply, state)
 end
 
 local function SetDisguise(ply, cmd, args)
-	if not IsValid(ply) or not (ply:IsTraitorTeam() or ply:IsActiveKiller()) then return end
+	if not IsValid(ply) or not (player.IsTraitorTeam(ply) or ply:IsActiveKiller()) then return end
 
 	if ply:HasEquipmentItem(EQUIP_DISGUISE) then
 		local state = #args == 1 and tobool(args[1])
